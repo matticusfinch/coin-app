078003c7aadbbfae728f07c969c3ec352e290f54
Matthew <m.ryanwall@gmail.com>
2020-06-05 21:24:57 -0600

    Added new mm

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const mm = [ MatButtonModule ];
+
+const mm = [ MatButtonModule, MatGridListModule, MatMenuModule, MatToolbarModule ];
 
 @NgModule({
   declarations: [],
