c4e4dc42ac49795b4ec6dd7496c568eb8c542127
Matthew <m.ryanwall@gmail.com>
2020-06-05 23:37:10 -0600

    Made navigation ugly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'; // CLI imports router
+import { RouterModule } from '@angular/router';
 import { appRoutes } from './app-routes';
 
-// configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
