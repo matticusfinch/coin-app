c2489588d69636ab72adbfe9d4261d5195428c51
Matthew <m.ryanwall@gmail.com>
2020-06-07 19:49:26 -0600

    Made home page shorter

--- src/app/main-landing/main-landing.module.ts
+++ src/app/main-landing/main-landing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
