5bdb831cb2771fda7f246e08525a4d15802c39a2
Matthew <m.ryanwall@gmail.com>
2020-06-10 20:27:48 -0600

    Cleaned up tailwind imports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyAuVWLI348STjrtPhRJQ3MOdCeT0dzJI9A',
+    authDomain: 'coinmanager.club',
+    databaseURL: 'https://coins-46e09.firebaseio.com',
+    projectId: 'coins-46e09',
+    storageBucket: 'coins-46e09.appspot.com',
+    messagingSenderId: '657499477077',
+    appId: '1:657499477077:web:7dfd7fa5b6e7438e5030dc',
+    measurementId: 'G-367P2P9P33'
+  }
 };
