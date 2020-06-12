3790e1de573d0630e6b4cc5eb3ea7affcfc5278f
solskira83 <rjs0523@p2es.com>
2020-06-11 22:54:20 -0600

    Add nav component

--- src/app/coin-manager/coin-manager.module.ts
+++ src/app/coin-manager/coin-manager.module.ts
@@ -3,11 +3,20 @@ import { LoginComponent } from './login/login.component'
 import { MaterialModule } from '../shared/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavComponent
   ],
   exports: [
     LoginComponent,
@@ -16,7 +25,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    CommonModule
   ]
 })
 export class CoinManagerModule { }
