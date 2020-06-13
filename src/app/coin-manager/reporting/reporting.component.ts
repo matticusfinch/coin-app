f906fd666743d1e94b5170c869983fc2fcbae6d8
Matthew <m.ryanwall@gmail.com>
2020-06-12 19:38:36 -0600

    #6 Add Account Drop Down

--- src/app/coin-manager/reporting/reporting.component.ts
+++ src/app/coin-manager/reporting/reporting.component.ts
@@ -13,7 +13,7 @@ export class ReportingComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
+          { title: 'Total Coins Collected', cols: 1, rows: 1 },
           { title: 'Card 2', cols: 1, rows: 1 },
           { title: 'Card 3', cols: 1, rows: 1 },
           { title: 'Card 4', cols: 1, rows: 1 }
@@ -21,7 +21,7 @@ export class ReportingComponent {
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Total Coins Collected ', cols: 2, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
         { title: 'Card 3', cols: 1, rows: 2 },
         { title: 'Card 4', cols: 1, rows: 1 }
