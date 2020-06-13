d1f2f6d2a4cf9dacf8d66f1f715c2d1de2830bf0
solskira83 <rjs0523@p2es.com>
2020-06-13 01:20:33 -0600

    #1 add Firebase UI

--- src/app/coin-manager/login/login.component.ts
+++ src/app/coin-manager/login/login.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import * as firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+
+firebase.initializeApp(environment.firebase);
+
 
 @Component({
   selector: 'app-login',
@@ -8,17 +15,30 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(
-    public authService: AuthService) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
+    const uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+          // signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this) <- alternate way to handle success if we need
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return true;
+        },
+      },
+      signInSuccessUrl: 'dashboard',
+      signInFlow: 'popup',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID, // <- I need to figure out this still
+      ],
+    };
 
-  tryGoogleLogin(){
-    this.authService.loginWithGoogle()
-    .then(res => {
-      console.log(res);
-    });
-  }
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
 }
