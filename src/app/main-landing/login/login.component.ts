251825ecf458d1b77c85c649facbea5dbbd7f75c
solskira83 <rjs0523@p2es.com>
2020-06-22 20:26:24 -0600

    move login logic to auth service

--- src/app/main-landing/login/login.component.ts
+++ src/app/main-landing/login/login.component.ts
@@ -1,47 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import * as firebase from 'firebase';
-import * as firebaseui from 'firebaseui';
-import { environment } from 'src/environments/environment';
-
-firebase.initializeApp(environment.firebase);
-
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy {
+export class LoginComponent implements OnInit {
 
   ui: any;
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
-    const uiConfig = {
-      callbacks: {
-        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-          // signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this) <- alternate way to handle success if we need
-          // User successfully signed in.
-          // Return type determines whether we continue the redirect automatically
-          // or whether we leave that to developer to handle.
-          return true;
-        }
-      },
-      signInSuccessUrl: 'dashboard',
-      // signInFlow: 'popup', // this make the auth popup, without it, it displays in page, which is better for mobile.
-      signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
-      ]
-    };
-
-    this.ui = new firebaseui.auth.AuthUI(firebase.auth());
-    this.ui.start('#firebaseui-auth-container', uiConfig);
-  }
-
-  ngOnDestroy() {
-    this.ui.delete();
-  }
+    this.authService.startUI('#firebaseui-auth-container');
+   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,57 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase from 'firebase';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+import { environment } from 'src/environments/environment';
+
+firebase.initializeApp(environment.firebase);
+
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+      // signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this) <- alternate way to handle success if we need
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    }
+  },
+  signInSuccessUrl: 'dashboard',
+  // signInFlow: 'popup', // this make the auth popup, without it, it displays in page, which is better for mobile.
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+  ]
+};
+
+
 
 @Injectable()
 export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth, public router: Router){}
+  userData: any;
+  ui: any;
 
-  async loginWithGoogle() {
-    await  this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    this.router.navigate(['dashboard']);
-  }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public afs: AngularFirestore) {
 
-  doLogout(){
-  }
+      this.afAuth.authState.subscribe(
+        user => {
+
+        }
+      );
+    }
+
+    startUI(container: string) {
+      this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+      this.ui.start('#firebaseui-auth-container', uiConfig);
+    }
+
+
 }
