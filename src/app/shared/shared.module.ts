801ed751dad5fd5803e60bdd2ac0e9f2fa57b51f
Matthew <m.ryanwall@gmail.com>
2020-07-02 00:05:20 -0600

    Fixed material import for popups

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { ArtistDialogComponent } from './info-dialogs/artist/artist-dialog.component';
+import { MonarchDialogComponent } from './info-dialogs/monarch/monarch-dialog.component';
+import { MintDialogComponent } from './info-dialogs/mint/mint-dialog.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
-  ]
+  ],
+  declarations: [
+    ArtistDialogComponent,
+    MonarchDialogComponent,
+    MintDialogComponent
+  ],
 })
 export class SharedModule { }
