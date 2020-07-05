0010b56b6757e823de8b68a0d1cdea951989a4de
Matthew <m.ryanwall@gmail.com>
2020-07-05 17:17:50 -0600

    Create user upon signup and say hi

--- src/app/coin-manager/reporting/reporting.component.ts
+++ src/app/coin-manager/reporting/reporting.component.ts
@@ -3,8 +3,7 @@ import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firest
 import { Observable } from 'rxjs';
 import { AngularFireAuth  } from '@angular/fire/auth';
 
-export interface Coin { totalCoins: number; }
-export interface Total { coinsCollected: number; }
+export interface Coin { totalCoins: number; coinsCollected: number; }
 
 @Component({
   selector: 'app-reporting',
@@ -12,16 +11,21 @@ export interface Total { coinsCollected: number; }
 })
 export class ReportingComponent implements OnInit {
   private coinDoc: AngularFirestoreDocument<Coin>;
-  private userDoc: AngularFirestoreDocument<Total>;
-  private user: firebase.User;
+  private userDoc: AngularFirestoreDocument<Coin>;
+  public user: firebase.User;
   coin: Observable<Coin>;
-  collected: Observable<Total>;
+  collected: Observable<Coin>;
   constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {
     afAuth.authState.subscribe(user => {
       this.user = user;
+      afs.collection('/users').doc(this.user.uid).set({
+        name: this.user.displayName,
+        coinsCollected: 43,
+        email: this.user.email
+      });
       this.coinDoc = afs.doc<Coin>('coins/newfoundland');
       this.coin = this.coinDoc.valueChanges();
-      this.userDoc = afs.collection<Total>('/users').doc(this.user.uid);
+      this.userDoc = afs.collection<Coin>('/users').doc(this.user.uid);
       this.collected = this.userDoc.valueChanges();
     });
 
